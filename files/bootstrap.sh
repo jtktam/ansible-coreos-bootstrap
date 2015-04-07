cbd3ff07a3571185a4f52a9b87d99820a59ade77
Joseph Tam <jtktam@users.noreply.github.com>
2015-04-07 11:55:26 -0400

    added comments / download URL variable

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -4,19 +4,24 @@ set -e
 
 cd
 
+## if already bootstrapped, quit
 if [[ -e $HOME/.bootstrapped ]]; then
   exit 0
 fi
 
+## define download version and URL
 PYPY_VERSION=2.4.0
+PYPY_URL=https://bitbucket.org/pypy/pypy/downloads
 
-wget -O - https://bitbucket.org/pypy/pypy/downloads/pypy-$PYPY_VERSION-linux64.tar.bz2 |tar -xjf -
+## download and untar pypy
+wget -O - $PYPY_URL/pypy-$PYPY_VERSION-linux64.tar.bz2 |tar -xjf -
 mv -n pypy-$PYPY_VERSION-linux64 pypy
 
 ## library fixup
 mkdir -p pypy/lib
 ln -snf /lib64/libncurses.so.5.9 $HOME/pypy/lib/libtinfo.so.5
 
+## create python shell stub
 mkdir -p $HOME/bin
 
 cat > $HOME/bin/python <<EOF
@@ -24,7 +29,9 @@ cat > $HOME/bin/python <<EOF
 LD_LIBRARY_PATH=$HOME/pypy/lib:$LD_LIBRARY_PATH exec $HOME/pypy/bin/pypy "\$@"
 EOF
 
+## make stub executable
 chmod +x $HOME/bin/python
 $HOME/bin/python --version
 
+## create .bootstrapped file to bypass
 touch $HOME/.bootstrapped
